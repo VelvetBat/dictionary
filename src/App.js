0e42e273c0cda791151f284b33b989aa24b6842f
VelvetBat <126163629+VelvetBat@users.noreply.github.com>
2024-07-11 21:21:11 +1000

    Error with alt img, attempt to fix !

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <header className="App-header"></header>
       Meow
-      <img src={Garden} className="garden" alt="garden picture" />
+      <img src={Garden} className="garden" alt="garden" />
     </div>
   );
 }
