54ebb4f679673b7cca0076b398239d871caed6b9
VelvetBat <126163629+VelvetBat@users.noreply.github.com>
2024-07-17 16:23:01 +1000

    Created dictionary.js + css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Garden from "./Garden.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Meow
-      <img src={Garden} className="garden" alt="garden" />
+      <div className="container">
+        <header className="App-header">
+          Meow
+          <img src={Garden} className="garden" alt="garden" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="app-footer">
+          Coded by Rebecca Z
+        </footer>
+      </div>
     </div>
   );
 }
