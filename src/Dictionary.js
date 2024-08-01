7d3e5def7d02f036d0ee9c4170c2a15ccbe1ca47
VelvetBat <126163629+VelvetBat@users.noreply.github.com>
2024-08-01 18:53:09 +1000

    Rendered meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState(" ");
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
-  function handleResponse(response) {}
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
 
   function search(event) {
-    event.preventDeafult();
+    event.preventDefault();
 
-    let key = "od4bd0df154ad3f5258f65d3a3c5bt61";
+    let key = `od4bd0df154ad3f5258f65d3a3c5bt61`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -24,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />;
     </div>
   );
 }
