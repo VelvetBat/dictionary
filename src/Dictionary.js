401e5de68725dc732baa12a448b20c59ad3b51dd
VelvetBat <126163629+VelvetBat@users.noreply.github.com>
2024-08-24 18:45:44 +1000

    Created photo request & added imageAPI

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 import "./Results.css";
 
@@ -8,15 +9,26 @@ export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     // documentation: https://dictionaryapi.dev/e
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    let apiUrl = `http://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const pexelsApiKey =
+      "od4bd0df154ad3f5258f65d3a3c5bt61";
+    let pexelsApiUrl = `http://api.shecodes.io/images/v1/search?query=${keyword}&key=${pexelsApiKey}`;
+    
+      axios.get(pexelsApiUrl).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -36,21 +48,19 @@ export default function Dictionary(props) {
   if (loaded) {
     return (
       <div className="Dictionary">
-
-          <form onSubmit={handleSubmit}>
-            <input
-              type="search"
-              onChange={handleKeywordChange}
-              autoFocus={true}
-              defaultValue={props.defaultKeyword}
-            />
-          </form>
-<div className="hint">
-  e.g cat, sunset, archery . . .
-</div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            onChange={handleKeywordChange}
+            autoFocus={true}
+            defaultValue={props.defaultKeyword}
+          />
+        </form>
+        <div className="hint">e.g cat, sunset, archery . . .</div>
         <div className="results">
           <Results results={results} />
         </div>
+        <Photos photos={photos} />
       </div>
     );
   } else {
@@ -58,5 +68,4 @@ export default function Dictionary(props) {
     return "Loading";
   }
 }
-
-
+// 8 mins left in vid
